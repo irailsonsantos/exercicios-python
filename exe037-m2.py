00ca183a4ecdeff043218a2c0267ddbbc8656091
Irailson <irailson@outlook.com>
2021-05-03 22:08:55 -0300

    primeiras melhorias no codigo

--- exe037-m2.py
+++ exe037-m2.py
@@ -1,7 +1,7 @@
 #entrada do numero e escolha da opção
 from time import sleep
 num = int(input('entre com um número para a conversão - '))
-opc = int(input('''entre com a opção Dejesada - 
+opc = int(input('''\33[1;30;41m###### entre com a opção Dejesada #######\33[m - 
 [ 0 ] - Sair do programa
 [ 1 ] - Binário -
 [ 2 ] - Octal -
@@ -10,8 +10,8 @@ Sua Opção -  '''))
 
 #validação da opção com retorno ao menu
 while opc >= 4:
-    print ('escolha opção correta')
-    opc = int(input('''entre com a opção Dejesada - 
+    print ('\33[1;31;40m    OPÇÃO INVÁLIDA     \33[m')
+    opc = int(input('''\33[1;;30;41m#####entre com a opção Dejesada###### \33[m - 
     0 - Sair do programa
     1 - Binário -
     2 - Octal -
@@ -22,9 +22,9 @@ if opc == 0:
     print ('saindo.......')
     sleep(2)
 if opc == 1:
-    print ('o numero em binário é  - {}'.format (bin(num)[2:]))
+    print ('o numero {} em binário é  - {}'.format (num, bin(num)[2:]))
 if opc == 2:
-    print ('o número em octal é -  {}'.format(oct(num)[2:]))
+    print ('o número {} em octal é -  {}'.format(num, oct(num)[2:]))
 if opc == 3:
-    print('o número em octal é -  {}'.format(hex(num)[2:]))
+    print('o número {} em HEXADECIMAL é -  {}'.format(num, hex(num)[2:]))
 
